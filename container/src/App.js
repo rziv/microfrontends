22dceaf50e978b2fb24713ce60e51be22d47153d
רן זיו | Ran Ziv <025544081@gov.il>
2021-07-18 11:12:34 +0300

    initial push

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp'
+
+export default  () => {return (<div>
+    <h2>main app</h2>
+
+   <hr/>
+   <MarketingApp />
+
+</div>)}
