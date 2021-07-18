6df785d76ac2459e11fdfdc18d221a5db4e11924
רן זיו | Ran Ziv <025544081@gov.il>
2021-07-18 14:26:20 +0300

    changes in component

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp'
 
 export default  () => {return (<div>
-    <h2> Main app</h2>
+    <h2>My Main app</h2>
 
    <hr/>
    <MarketingApp />
