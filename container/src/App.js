0168ec8b6a5805db2846f0b702bd9db441794983
רן זיו | Ran Ziv <025544081@gov.il>
2021-07-26 09:59:58 +0300

    rendomizing class names

--- container/src/App.js
+++ container/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header'
 
 const generateClassName = createGenerateClassName(
     {
-        productionPrefix: 'marketing'
+        productionPrefix: 'container'
     }
 )
 
