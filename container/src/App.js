27c60327646ac6510f02b8e8a886fb673cb464e3
רן זיו | Ran Ziv <025544081@gov.il>
2021-07-26 08:53:16 +0300

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
-export default  () => {return (<div>
-    <h2>My Main app</h2>
-
-   <hr/>
-   <MarketingApp />
-
-</div>)}
+export default () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    )
+}
