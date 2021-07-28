78d94aeba8f6b52d3263a9b578506aeab0b42bfa
רן זיו | Ran Ziv <025544081@gov.il>
2021-07-28 10:46:47 +0300

    adding auth workflow

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Progress from './components/Progress'
 import Header from './components/Header'
+
+
 
 const generateClassName = createGenerateClassName(
     {
@@ -14,14 +16,17 @@ const MarketingAppLazy = lazy(() => import('./components/MarketingApp'))
 const AuthAppLazy = lazy(() => import('./components/AuthApp'))
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false)
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
-                    <Suspense fallback={<Progress/>}>
+                    <Header onSignOut={()=>setIsSignedIn(false)} isSignedIn={isSignedIn} />
+                    <Suspense fallback={<Progress />}>
                         <Switch>
-                            <Route path='/auth' component={AuthAppLazy} />
+                            <Route path='/auth' >
+                                <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
                             <Route path='/' component={MarketingAppLazy} />
                         </Switch>
                     </Suspense>
--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -1,11 +1,12 @@
 import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'
-export default () => {
+export default ({onSignIn}) => {
     const ref = useRef(null)
     const history = useHistory()
     useEffect(() => {
         const { onParentNavigate } = mount(ref.current, {
+            onSignIn,
             initialPath: history.location.pathname,
             onNavigate: ({ pathname: nextPathname }) => {
                 const { pathname } = history.location
